dbb7456e828b78300b2e9a5e6846628574d3345f
Masato Tanaka <tanaka.masato@jp.panasonic.com>
2025-09-25 05:47:13 +0000

    feat: add extra query parameters for Azure identity provider in AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -16,8 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 // wrap the application with AuthProvider
 root.render(
   <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
+    <AuthProvider {...cognitoAuthConfig} extraQueryParams={{ identity_provider: "Azure" }}>
+      {/* </AuthProvider><AuthProvider {...cognitoAuthConfig} extraQueryParams={{ identity_provider: "Okta" }}> */}
       <App />
-    </AuthProvider>
+      </AuthProvider>
   </React.StrictMode>
 );
